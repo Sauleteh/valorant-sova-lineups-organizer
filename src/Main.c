1188cb0db5313e7b61a9058a85c7b6430883f4cc
Sauleteh <sauljgz21@yahoo.es>
2024-07-21 16:15:10 +0200

    Programa convertido al inglés + TODO list

--- src/Main.c
+++ src/Main.c
@@ -13,20 +13,20 @@ int main() {
     // Obtener el manejador de la entrada estándar
     hStdin = GetStdHandle(STD_INPUT_HANDLE);
     if (hStdin == INVALID_HANDLE_VALUE) {
-        printf("Error al obtener el manejador de la consola\n");
+        printf("Error obtaining input handle\n");
         return 1;
     }
 
     // Guardar el modo de entrada original para restaurarlo al final
     if (!GetConsoleMode(hStdin, &fdwSaveOldMode)) {
-        printf("Error al obtener el modo de la consola\n");
+        printf("Error obtaining console mode\n");
         return 1;
     }
 
     // Configurar el modo de entrada para habilitar la entrada del ratón y procesar Ctrl+C
     fdwMode = ENABLE_EXTENDED_FLAGS | ENABLE_WINDOW_INPUT | ENABLE_MOUSE_INPUT | ENABLE_PROCESSED_INPUT;
     if (!SetConsoleMode(hStdin, fdwMode)) {
-        printf("Error al configurar el modo de la consola\n");
+        printf("Error setting console mode\n");
         return 1;
     }
 
@@ -37,7 +37,7 @@ int main() {
     while (1) {
         // Esperar a que ocurra un evento en la consola
         if (!ReadConsoleInput(hStdin, irInBuf, 128, &cNumRead)) {
-            printf("Error al leer la entrada de la consola\n");
+            printf("Error reading console input\n");
             return 1;
         }
 
@@ -81,3 +81,17 @@ int main() {
 
     return 0;
 }
+
+/**
+ * TODO list:
+ * - [ ] Detectar qué carpetas existen en el directorio actual para mostrarlas como opciones en el menú
+ * - [ ] Si se detecta más de un monitor, se debe preguntar cuál es el monitor que se está utilizando para el Valorant (esto se pregunta al principio del programa y se guardará en un archivo de configuración)
+ * - [ ] Al seleccionar un mapa, se debe guardar qué se seleccionó
+ * - [ ] Después de escoger un mapa, se debe elegir en qué parte del mapa se realizará el lineup (A, B, C u Other)
+ * - [ ] Si no está guardado en la config, se debe preguntar al usuario qué tecla utilizar para realizar la captura de pantalla y se guarda posteriormente en el archivo de configuración
+ * - [ ] Una vez seleccionado el mapa y la parte, se pedirá al usuario que saque tres capturas de pantalla
+ *       - [ ] La primera captura será mirar hacia dónde hay que colocarse
+ *       - [ ] La segunda captura será mirar hacia dónde se debe lanzar la habilidad
+ *       - [ ] La tercera captura será ver dónde cae la habilidad
+ *       - [ ] Las capturas de pantalla NO se cogen del portapapeles; se detecta cuándo se presiona imprimir pantalla y entonces se procesa la pantalla (esto se hace porque si hay más de un monitor, el portapapeles tendrá la captura de todos los monitores)
+ */
